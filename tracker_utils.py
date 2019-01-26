3c6fbe4758da40a736d9834edeee5cdac63b7a5d
joeyism <sham.joey@gmail.com>
2019-01-26 17:03:36 +0000

    output; updated remove older tracks

--- tracker_utils.py
+++ tracker_utils.py
@@ -2,6 +2,4 @@ from deep_sort.track import TrackState
 
 
 def remove_deleted_tracks(tracker):
-    for track in tracker.tracks:
-        if track.state == TrackState.Deleted:
-            del track
+    tracker.tracks = [track for track in tracker.tracks if track.state != TrackState.Deleted]
